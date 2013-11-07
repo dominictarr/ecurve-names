be8dfea7c8d634932a948e5f2f7e0f877d92b6fe
JP Richardson <jprichardson@gmail.com>
2013-11-06 20:28:15 -0600

    v0.0.1

--- test/ecc-named.test.js
+++ test/ecc-named.test.js
@@ -1,6 +1,15 @@
+var getSECCurveByName = require('../lib/ecc-named')
+
 require('terst')
 
-
+describe('+ getSECCurveByName(curveName)', function() {
+  describe('> when the bitcoin curve is passed', function() {
+    it('should return the proper curve', function() {
+      var curve = getSECCurveByName('secp256k1')
+      T (curve)
+    })
+  })
+})
 
 
 
